c4aef11c220eb2d2457e1a47a0e4c608ab408af5
Lucas Medeiros <=>
2025-06-30 11:38:35 -0300

    feat: last class adding mock in tests

--- tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/todos.service.spec.ts
+++ tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/todos.service.spec.ts
@@ -2,18 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { TodosService } from './todos.service';
 import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment.development';
+import { TODO_STRING, TODOS_STRING } from '../../../server/db-data';
 
 describe('TodosService', () => {
   let todosService: TodosService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         TodosService,
-        provideHttpClient()
+        provideHttpClient(),
+        provideHttpClientTesting()
       ]
     });
     todosService = TestBed.inject(TodosService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -26,14 +32,22 @@ describe('TodosService', () => {
       expect(todos.length).toEqual(200);
 
       const todo = todos.find(todo => todo.id === 15);
-      expect(todo?.title).toEqual('accusamus eos facilis sint et aut voluptatem')
-    })
+      expect(todo?.title).toEqual('ab voluptatum amet voluptas')
+    });
+
+    const req = httpTestingController.expectOne(environment.apiUrl + 'todos');
+    expect(req.request.method).toEqual('GET');
+    req.flush(JSON.parse(TODOS_STRING))
   });
 
    it('should return todo for id', async () => {
-    const todo = await todosService.getById(10).toPromise();
+    todosService.getById(3).subscribe(todo => {
+      expect(todo).toBeTruthy();
+      expect(todo.id).toEqual(3);
+    })
 
-    expect(todo).toBeTruthy();
-    expect(todo?.id).toEqual(10);
+  const req = httpTestingController.expectOne(environment.apiUrl + 'todos/3');
+    expect(req.request.method).toEqual('GET');
+    req.flush(JSON.parse(TODO_STRING));
   });
 });
