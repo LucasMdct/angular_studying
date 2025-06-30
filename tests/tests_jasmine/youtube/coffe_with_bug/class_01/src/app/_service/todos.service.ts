3e0284f1ae158a789bd076a9e149dcc8a56a0449
Lucas Medeiros <=>
2025-06-30 11:14:34 -0300

    feat: implementing real requests with jsonplaceholder

---
+++ tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/todos.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosService {
+
+  baseUrl: string = environment.apiUrl;
+
+  constructor(
+   private http: HttpClient
+  ) { }
+
+  getAll() {
+    return this.http.get(this.baseUrl + 'todos')
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      )
+  }
+
+   getById(id: number) {
+    return this.http.get(this.baseUrl + 'todos/' + id)
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      )
+  }
+}
--- tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/app.component.ts
+++ tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodosService } from './_service/todos.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'class_01';
+
+  constructor(private todoService: TodosService) {
+
+  }
+
+  ngOnInit(): void {
+    this.todoService.getAll().subscribe((response) => {
+      console.log(response);
+    })
+
+    this.todoService.getById(1).subscribe((response) => {
+      console.log(response);
+    })
+  }
 }
