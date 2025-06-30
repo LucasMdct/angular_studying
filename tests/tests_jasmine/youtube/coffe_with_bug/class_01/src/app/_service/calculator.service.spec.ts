f1ba4ba033312a2b3ff16c0ffa2835062f3b1d97
Lucas Medeiros <=>
2025-06-30 10:40:21 -0300

    feat: testing a function of the calculator service class, specifically the sum function

--- tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/calculator.service.spec.ts
+++ tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/calculator.service.spec.ts
@@ -2,32 +2,29 @@ import { TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
-// Agrupador de testes describe
 describe('CalculatorService', () => {
   let service: CalculatorService;
 
-  // TODO To avoid repeating so much code within the functions, we use the before each
   beforeEach(() => {
-    // configures a test module. It's like setting up a “mini Angular” just for testing.
     TestBed.configureTestingModule({
       providers: [CalculatorService],
-      // you are saying:
-      // “In this test environment, I want to make the CalculatorService
-      //  service available.”
+
     });
-    service = TestBed.inject(CalculatorService); // getting a calculator service instance
+    service = TestBed.inject(CalculatorService);
   });
 
 
-  //TODO this is executed after each test
-  //  afterEach(() => {
-
-  // })
 
   it('should be created', () => {
-    //  TestBed.configureTestingModule({});
-    // service = TestBed.inject(CalculatorService);
     expect(service).toBeTruthy();
   });
+
+  it('should sum correctly with two numbers', () => {
+    expect(service).toBeTruthy();
+    const result = service.calc(5,8, 'add');
+
+    expect(result).toBe(13);
+  })
+
 
 });
