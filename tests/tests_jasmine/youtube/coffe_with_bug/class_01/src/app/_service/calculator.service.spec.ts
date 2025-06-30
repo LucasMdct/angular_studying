fa67f7b129d9f79b5762d8999d7c1c9e47de3939
Lucas Medeiros <=>
2025-06-30 10:48:50 -0300

    feat: testing all the functions of the calculator

--- tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/calculator.service.spec.ts
+++ tests/tests_jasmine/youtube/coffe_with_bug/class_01/src/app/_service/calculator.service.spec.ts
@@ -26,5 +26,25 @@ describe('CalculatorService', () => {
     expect(result).toBe(13);
   })
 
+    it('should div correctly with two numbers', () => {
+    expect(service).toBeTruthy();
+    const result = service.calc(20,5, 'div');
+
+    expect(result).toBe(4);
+  })
+
+    it('should mul correctly with two numbers', () => {
+    expect(service).toBeTruthy();
+    const result = service.calc(5,8, 'mul');
+
+    expect(result).toBe(40);
+  })
+
+    it('should sub correctly with two numbers', () => {
+    expect(service).toBeTruthy();
+    const result = service.calc(15,0, 'sub');
+
+    expect(result).toBe(15);
+  })
 
 });
